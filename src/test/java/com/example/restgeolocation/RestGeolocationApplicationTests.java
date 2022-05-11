5849150d1c22db7f252db8be8da5c19c1c4d23ed
Tomasz Rafalski <tomekrafalski1@gmail.com>
2022-05-11 02:05:57 +0200

    Update RestGeolocationApplicationTests.java

--- src/test/java/com/example/restgeolocation/RestGeolocationApplicationTests.java
+++ src/test/java/com/example/restgeolocation/RestGeolocationApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.restgeolocation;
 
+import com.example.restgeolocation.geolocation.entity.Geolocation;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootTest
 class RestGeolocationApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
